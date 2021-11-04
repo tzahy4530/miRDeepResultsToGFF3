10a77dbd7a439069c88258f54f2b5550cb8c0e6f
tzahy4530 <55870993+tzahy4530@users.noreply.github.com>
2021-11-04 16:49:07 +0200

    added option to save fasta file of the gff3

--- miRDeepResultsToGFF3.py
+++ miRDeepResultsToGFF3.py
@@ -20,12 +20,20 @@ def filterInputs(inputs_arr, score_threshold, true_positive_threshold, exclude_c
 
     filtered_inputs_arr = []
 
-
+    file_count = 1
     for input in inputs_arr:
+        total_reads = len(input.index)
+        total_reads_left = total_reads
+        sys.stdout.write(f'Summary of Input File Number {file_count}:\n'
+              f'\tTotal Reads Before Filtering: {total_reads}\n')
         if score_threshold is not None:
             input['total read count'] = pd.to_numeric(input['total read count'])
             input['miRDeep2 score'] = pd.to_numeric(input['miRDeep2 score'])
             input = input[(input['total read count'] >= exclude_counts) | (input['miRDeep2 score'] >= score_threshold)]
+            total_reads_left = len(input.index)
+            filtered_percent = "%.2f" % (((total_reads-total_reads_left)/total_reads)*100)
+            sys.stdout.write(f'\tTotal Reads Filtered by Score: {total_reads-total_reads_left} ({filtered_percent}%)\n')
+
         if true_positive_threshold is not None:
             try:
                 # Novel MicroRNA
@@ -39,6 +47,19 @@ def filterInputs(inputs_arr, score_threshold, true_positive_threshold, exclude_c
                   axis=1))
 
             input = input[input['true positive probability'] >= true_positive_threshold]
+            total_reads_left_after_true_positive_filtering = len(input.index)
+
+            filtered_percent = "%.2f" % (((total_reads_left - total_reads_left_after_true_positive_filtering) / total_reads_left)*100)
+
+            sys.stdout.write(
+                f'\tTotal Reads Filtered by True-Positive Probability:'
+                f' {total_reads_left - total_reads_left_after_true_positive_filtering} ({filtered_percent}%)\n')
+
+            total_reads_left = total_reads_left_after_true_positive_filtering
+
+        filtered_percent = "%.2f" % ((total_reads_left / total_reads)*100)
+        sys.stdout.write(f'\tTotal Reads Left After Filtering: {total_reads_left} ({filtered_percent}%)\n')
+        file_count += 1
         filtered_inputs_arr.append(input)
 
     return filtered_inputs_arr
@@ -94,12 +115,24 @@ def readMirbaseResults(input_path):
 
     return inputs
 
+def getSeqId(row):
+    try:
+        seq_id = row['provisional id']  # *
 
-def run(inputs, output, threshold_tp, threshold_s, exclude_c):
+    except:
+        # That's mean that belong to known microRNA
+        seq_id = row["mature miRBase miRNA"]
+        seq_id = seq_id.replace('-3p', '')
+        seq_id = seq_id.replace('-5p', '')
+
+    return seq_id
+
+def run(inputs, output, threshold_tp, threshold_s, exclude_c, fasta_path):
     f"""
     This function create gff3 file from the inputs, This function will add to the miRNA ID -m / -s if its mature/star,
     -number which the number its the frequency of this seq among its type (mature/star)
-    :param exclude_c: Int - exclude rows from the filtering when total counts higher than exclude_c .
+    :param fasta_path: String - fasta file will created in this path.
+    :param exclude_c: Int - exclude rows from the filtering when total counts higher than exclude_c.
     :param threshold_s: Float - threshold for score.
     :param threshold_tp: Float - threshold for the true positive estimate.
     :param inputs: Array - with DataFrames of miRDeep results.csv predict tables.
@@ -112,31 +145,26 @@ def run(inputs, output, threshold_tp, threshold_s, exclude_c):
 
     filtered_input = filterInputs(inputs, threshold_s, threshold_tp, exclude_c)
 
+    if fasta_path is not None:
+        fasta_file = ''
+        open(fasta_path, 'w').close()
+
     for input in filtered_input:
         for index, row in input.iterrows():
             details = row['precursor coordinate']
-            name = details.split(':')[0]  # *
+            name = details.split(':')[0]
             positions = details.split(':')[1]
-            strand = details.split(':')[2]  # *
-
-            try:
-                seq_id = row['provisional id']  # *
-
-            except:
-                # That's mean that belong to known microRNA
-                seq_id = row["mature miRBase miRNA"]
-                seq_id = seq_id.replace('-3p', '')
-                seq_id = seq_id.replace('-5p', '')
-
+            strand = details.split(':')[2]
+            seq_id = getSeqId(row)
             star_seq = row['consensus star sequence']
             mature_seq = row['consensus mature sequence']
-            hairpin = row['consensus precursor sequence']  # *
+            hairpin = row['consensus precursor sequence']
 
             star_position = hairpin.index(star_seq)
             mature_position = hairpin.index(mature_seq)
 
-            seq5p_id = seq_id + '-5p'  # *
-            seq3p_id = seq_id + '-3p'  # *
+            seq5p_id = seq_id + '-5p'
+            seq3p_id = seq_id + '-3p'
 
             if star_position > mature_position:
                 seq5p = row['consensus mature sequence']  # *
@@ -154,48 +182,49 @@ def run(inputs, output, threshold_tp, threshold_s, exclude_c):
                 seq5p_id += f'-m-{seq5p_freq}'
                 seq3p_id += f'-s-{seq3p_freq}'
 
+
+            if fasta_path is not None:
+                if seq5p != '-':
+                    fasta_file += f'>{seq5p_id}\n{seq5p}\n'
+
+                if seq3p != '-':
+                    fasta_file += f'>{seq3p_id}\n{seq3p}\n'
+
+                if len(fasta_file) > 100000:
+                    with open(fasta_path, 'a+') as f:
+                        f.write(fasta_file)
+                    fasta_file = ''
+
             start = int(positions.split('..')[0]) + 1
             end = int(positions.split('..')[1])
 
             gff_row = [[name, '.', 'pre_miRNA', start, end, '.', strand, '.', f'ID={seq_id}']]
 
             if strand == '+':
-                try:
-                    if seq5p == '-':
-                        raise ('5p Not Exists')
+                if seq5p != '-':
                     offset5p = len(hairpin.split(seq5p)[0])
                     start5p = start + offset5p
                     end5p = start + offset5p + len(seq5p) - 1
                     gff_row.append([name, '.', 'miRNA', start5p, end5p, '.', strand, '.', f'ID={seq5p_id}'])
-                except:
-                    pass
 
-                try:
-                    if seq3p == '-':
-                        raise ('3p Not Exists')
+                if seq3p != '-':
                     offset3p = len(hairpin.split(seq3p)[0])
                     start3p = start + offset3p
                     end3p = start + offset3p + len(seq3p) - 1
                     gff_row.append([name, '.', 'miRNA', start3p, end3p, '.', strand, '.', f'ID={seq3p_id}'])
-                except:
-                    pass
 
             else:
-                try:
+                if seq5p != '-':
                     offset5p = len(hairpin.split(seq5p)[0])
                     end5p = end - offset5p
                     start5p = end - offset5p - len(seq5p) + 1
                     gff_row.append([name, '.', 'miRNA', start5p, end5p, '.', strand, '.', f'ID={seq5p_id}'])
-                except:
-                    pass
 
-                try:
+                if seq3p != '-':
                     offset3p = len(hairpin.split(seq3p)[0])
                     end3p = end - offset3p
                     start3p = end - offset3p - len(seq3p) + 1
                     gff_row.append([name, '.', 'miRNA', start3p, end3p, '.', strand, '.', f'ID={seq3p_id}'])
-                except:
-                    pass
 
             miRNAs = pd.DataFrame(gff_row, columns=gff3_columns)
 
@@ -203,6 +232,10 @@ def run(inputs, output, threshold_tp, threshold_s, exclude_c):
 
     with open(output, 'w') as file:
         file.write(version)
+
+    if fasta_path is not None:
+        with open(fasta_path, 'a+') as f:
+            f.write(fasta_file)
 
     gff3.to_csv(output, index=False, header=False, mode="a", sep='\t')
 
@@ -214,6 +247,7 @@ if __name__ == '__main__':
     csv_save = False
     threshold_tp = None
     threshold_s = None
+    fasta_path = None
     exclude_c = None
     args = []
 
@@ -228,6 +262,8 @@ if __name__ == '__main__':
             threshold_tp = sys.argv[i + 1]
         elif arg == '--filter-s':
             threshold_s = sys.argv[i + 1]
+        elif arg == '--create-fasta':
+            fasta_path = sys.argv[i + 1]
         elif arg == '--exclude-c':
             exclude_c = sys.argv[i + 1]
         elif arg == '--csv-save':
@@ -244,7 +280,8 @@ if __name__ == '__main__':
                   f' --filter-s <float> : threshold for score, default: None.\n'
                   f' --exclude-c <int> : term to ignore the score filter threshold if total counts are higher, default: '
                   f'None.\n '
-                  f' --csv-save : will save the inner tables of miRDeep2 output results as csv.\n')
+                  f' --csv-save : will save the inner tables of miRDeep2 output results as csv.\n'
+                  f' --create-fasta <path>: create fasta file from the gff3 table.\n')
             sys.exit()
         i += 2
 
@@ -269,4 +306,4 @@ if __name__ == '__main__':
     if exclude_c is not None:
         exclude_c = int(exclude_c)
 
-    run(inputs, output, threshold_tp, threshold_s, exclude_c)
+    run(inputs, output, threshold_tp, threshold_s, exclude_c, fasta_path)
